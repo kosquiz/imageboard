a42eb520b3c088112c182820f4f36f5b2e328682
F.Stilo <F.Stilo@klara-oppenheimer-schule.de>
2019-09-12 14:43:43 +0200

    erster tag

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// Include router class
+include('Classes/Route.php');
+
+// Add base route (startpage)
+Route::add('/',function(){
+    echo 'Welcome :-)';
+});
+
+// Simple test route that simulates static html file
+Route::add('/test.html',function(){
+    echo 'Hello from test.html';
+});
+
+// Post route example
+Route::add('/contact-form',function(){
+    echo '<form method="post"><input type="text" name="test" /><input type="submit" value="send" /></form>';
+},'get');
+
+// Post route example
+Route::add('/contact-form',function(){
+    echo 'Hey! The form has been sent:<br/>';
+    print_r($_POST);
+},'post');
+
+// Accept only numbers as parameter. Other characters will result in a 404 error
+Route::add('/foo/([0-9]*)/bar',function($var1){
+    echo $var1.' is a great number!';
+});
+
+Route::run('/');
