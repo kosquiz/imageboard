27211009b9b893841b1f8aa6b3f7c34689a6b330
F.Stilo <F.Stilo@klara-oppenheimer-schule.de>
2019-10-07 08:51:55 +0200

    test

--- Classes/Controller.php
+++ Classes/Controller.php
@@ -1,12 +1,16 @@
 <?php
 
+require_once('Database.php');
+require_once('Output.php');
+
 class Controller{
 
     private $out;
     private $db;
 
     public function __construct(){
-
+        $this->out = new Output();
+        $this->db = new Database();
     }
 
     public function indexAction(){
--- index.php
+++ index.php
@@ -1,30 +1,37 @@
 <?php
 // Include router class
-include('Classes/Route.php');
+require_once('Classes/Route.php');
+require_once('Classes/Controller.php');
+
+//TODO CHANGE LATER TO ''
+$prefix = "/imgboard";
+
+$ctrl = new Controller();
 
 // Add base route (startpage)
-Route::add('/',function(){
-    $ctrl->indexAction()
+Route::add($prefix.'/',function(){
+    global $ctrl;
+    $ctrl->indexAction();
 });
 
 // Simple test route that simulates static html file
-Route::add('/test.html',function(){
+Route::add($prefix.'/test.html',function(){
     echo 'Hello from test.html';
 });
 
 // Post route example
-Route::add('/contact-form',function(){
+Route::add($prefix.'/contact-form',function(){
     echo '<form method="post"><input type="text" name="test" /><input type="submit" value="send" /></form>';
 },'get');
 
 // Post route example
-Route::add('/contact-form',function(){
+Route::add($prefix.'/contact-form',function(){
     echo 'Hey! The form has been sent:<br/>';
     print_r($_POST);
 },'post');
 
 // Accept only numbers as parameter. Other characters will result in a 404 error
-Route::add('/foo/([0-9]*)/bar',function($var1){
+Route::add($prefix.'/foo/([0-9]*)/bar',function($var1){
     echo $var1.' is a great number!';
 });
 
