0cc46b4d51c5eff3beabeb590a9697cb48a6c819
F.Stilo <F.Stilo@klara-oppenheimer-schule.de>
2019-10-08 08:42:31 +0200

    database

--- Classes/Database.php
+++ Classes/Database.php
@@ -7,4 +7,11 @@ class Database{
     public function __construct(){
         $this->db = new PDO("mysql:dbname=imgboard;host=127.0.0.1;charset=utf8", "root");
     }
+
+    public function insertUser($user, $pass){
+        $sql = $this->db->prepare("INSERT INTO users(email,password) VALUES(?,?)");
+        $sql->execute($user,$pass);
+        $this->db->commit();
+        return $this->db->lastInsertId();
+    }
 }
